351fe1f1bab75d193ca6485dac51638aa3d2c608
JayPixl <129111077+JayPixl@users.noreply.github.com>
2023-03-29 23:09:03 -0500

    Finished, linked to HTML page

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 const port = 3000;
 
 const server = http.createServer( function (req, res) {
 
-    res.write("<h1>Hello Node!</h1>");
-    res.end();
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', function (error, data) {
+
+        if (error) {
+            res.writeHead(404);
+            res.write('Error: File Not Found');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
 
 })
 
